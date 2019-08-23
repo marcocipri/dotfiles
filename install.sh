c6d923b48dc6eb304d7a62855f7945c425bea533
marco cipri <marcocipri@gmail.com>
2019-08-23 19:30:42 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,18 +7,16 @@
 ########## Variables
 
 dir=~/dotfiles                    # dotfiles directory
-files="bashrc vimrc vim zshrc"    # list of files/folders to symlink in homedir
-
+files="bashrc vimrc zshrc config"    # list of files/folders to symlink in homedir
 ##########
 
-
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
-for file in $files; do
-    echo "Copy to $file in home directory."
-    cp $dir/$file ~/.$file
-done
+    echo "Changing to the $dir directory"
+    cd $dir
+    echo "...done"
+   
+    # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+    for file in $files; do
+        echo "Copy to $file in home directory."
+        cp -r $dir/$file ~/.$file
+    done
